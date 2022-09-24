b788e1f9ebf22a98cbf33aeeed7433d7591715a9
bharatvitkar <79301656+bharatvitkar@users.noreply.github.com>
2022-09-24 09:55:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+x=0;
+y=0;
+
+draw_circle="";
+draw_rectangle="";
+
+var speechrecognition=window.webkitSpeechRecognition;
+var recognition=new speechrecognition();
+
+function start()
+{
+    document.getElementById("status").innerHTML="system is listing please speak";
+    recognition.start();
+}
+
+recognition.onresult=function(event)
+{
+    console.log(event);
+    var content=event.results[0][0].transcript;
+    console.log(content);
+    document.getElementById("status").innerHTML="the speech has been recognized as: "+content;
+    if(content=="circle")
+    {
+        x=Math.floor(Math.random()*900);
+        y=Math.floor(Math.random()*600);
+        document.getElementById("status").innerHTML="started drawing circle";
+        draw_circle="set";
+    }
+    if(content=="rectangle")
+    {
+        x=Math.floor(Math.random()*900);
+        y=Math.floor(Math.random()*600);
+        document.getElementById("status").innerHTML="started drawing rectangle";
+        draw_rectangle="set";
+    }
+}
+function setup()
+{
+    canvas=createCanvas(900,600);
+}
+function draw()
+{
+    if(draw_circle=="set")
+    {
+        radius=Math.floor(Math.random()*100);
+        circle(x,y,radius);
+        document.getElementById("status").innerHTML="circle is drawn";
+        draw_circle="";
+    }
+    if(draw_rectangle=="set")
+    {
+        rect(x,y,70,40);
+        document.getElementById("status").innerHTML="rectangle is drawn";
+        draw_rectangle="";
+    }
+}
